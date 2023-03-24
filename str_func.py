85b9a8c7fc666f2d023ac0164de9b8c45ed074d0
yury-yury <yury_yury@mail.ru>
2023-03-24 15:43:51 +0300

    hotfix

--- str_func.py
+++ str_func.py
@@ -1,6 +1,7 @@
 def make_upper(source_string: str) -> str:
     """
-     The make_upper function takes a string as an argument, converts the character case to uppercase,
+     The make_upper function takes a string as an argument,
+     converts the character case to uppercase,
      and returns the result as a string.
     """
     return source_string.upper()
@@ -8,7 +9,8 @@ def make_upper(source_string: str) -> str:
 
 def make_title(source_string: str) -> str:
     """
-     The function takes a string as an argument, capitalizes the first letters of each word,
+     The function takes a string as an argument,
+     capitalizes the first letters of each word,
      and returns the result as a string.
     """
     return source_string.title()
