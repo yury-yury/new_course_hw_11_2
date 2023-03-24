348599636f4c3e521cdf48077f4b991570cfd51f
yury-yury <yury_yury@mail.ru>
2023-03-24 13:12:12 +0300

    create func make_title

--- str_func.py
+++ str_func.py
@@ -1,2 +1,14 @@
 def make_upper(source_string: str) -> str:
+    """
+     The make_upper function takes a string as an argument, converts the character case to uppercase,
+     and returns the result as a string.
+    """
     return source_string.upper()
+
+
+def make_title(source_string: str) -> str:
+    """
+     The function takes a string as an argument, capitalizes the first letters of each word,
+     and returns the result as a string.
+    """
+    return source_string.title()
